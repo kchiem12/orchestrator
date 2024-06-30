5e6019a9d846860a47ac359b3fe5b169e00a5f02
Kenneth Chiem <kc696@cornell.edu>
2024-06-30 18:46:14 -0500

    create skeleton of worker

---
+++ worker/worker.go
@@ -0,0 +1,32 @@
+package worker
+
+import (
+	"fmt"
+	"orchestrator/task"
+
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+)
+
+type Worker struct {
+	Name      string
+	Queue     queue.Queue
+	Db        map[uuid.UUID]task.Task
+	Taskcount int
+}
+
+func (w *Worker) CollectStats() {
+	fmt.Println("Collecting stats")
+}
+
+func (w *Worker) RunTask() {
+	fmt.Println("Running task")
+}
+
+func (w *Worker) StartTask() {
+	fmt.Println("Starting task")
+}
+
+func (w *Worker) StopTask() {
+	fmt.Println("Stopping task")
+}
