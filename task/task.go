2bbcd1a79daca2888a9f9a2bd350d1a3486839c6
Kenneth Chiem <kc696@cornell.edu>
2024-07-01 22:41:41 -0500

    add a config type for tasks

--- task/task.go
+++ task/task.go
@@ -37,3 +37,16 @@ type TaskEvent struct {
 	Timestamp time.Time
 	Task      Task
 }
+
+type Config struct {
+	Name          string
+	AttachStdin   bool
+	AttachStdout  bool
+	AttachStderr  bool
+	Cmd           []string
+	Image         string
+	Memory        int64
+	Disk          int64
+	Env           []string
+	RestartPolicy string
+}
